a337236674d35c0e6020b060e7b7c8777e6047b6
KojiTakahara <plateau1205@gmail.com>
2015-11-26 22:45:10 +0900

    angular追加

---
+++ static/js/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var app = angular.module('app', [
+]);
+
+app.controller('indexCtrl', ['$scope', '$http', '$sce', '$window', function($scope, $http, $sce, $window) {
+
+
+}]);
+
+app.config(['$httpProvider', '$locationProvider', function($httpProvider, $locationProvider) {
+  $httpProvider.defaults.headers.common = {'X-Requested-With': 'XMLHttpRequest'};
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
+  $httpProvider.defaults.transformRequest = function(data) {
+    if (data === undefined) {
+      return data;
+    }
+    return $.param(data);
+  }
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}]);
