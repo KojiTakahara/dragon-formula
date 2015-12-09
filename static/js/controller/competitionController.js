f03b1be0767e62d3146c841b6a8ef374e56ea973
koji takahara <plateau1205@gmail.com>
2015-12-09 20:28:46 +0900

    問題ページのhtmlを修正

--- static/js/controller/competitionController.js
+++ static/js/controller/competitionController.js
@@ -2,7 +2,10 @@
 
 var app = angular.module('competitionCtrl', []);
 app.controller('competitionCtrl', ['$scope', '$http', '$sce', '$window', 'questionService', function($scope, $http, $sce, $window, questionService) {
-
+	$scope.questions = [];
+	for (var i = 0; i < 25; i++) {
+		$scope.questions.push(i + 1);
+	}
 	var init = function() {
     	questionService.search().then(function(data) {
 			$scope.list = data;
--- static/js/controller/indexController.js
+++ static/js/controller/indexController.js
@@ -3,10 +3,6 @@
 var app = angular.module('indexCtrl', []);
 app.controller('indexCtrl', ['$scope', '$http', '$sce', '$window', function($scope, $http, $sce, $window) {
   $scope.isStarted = false;
-  $scope.questions = [];
-  for (var i = 0; i < 25; i++) {
-    $scope.questions.push(i + 1);
-  }
   $scope.start = function() {
     $scope.isStarted = true;
   };
