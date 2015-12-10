117dea04da35f76e8c8bcb4066f4d5ee415b14e9
KojiTakahara <plateau1205@gmail.com>
2015-12-11 08:44:29 +0900

    jsの適用

---
+++ data.sh
@@ -0,0 +1,1 @@
+wget --post-data="choice1Content=イベントのフォーマットは構築またはリミテッドで開催可能である。&choice2Content=イベントのフォーマットは構築のみ開催可能である。&choice3Content=イベントのフォーマットはリミテッドのみ開催可能である。&choice4Content=イベントはどのようなフォーマットでも開催可能である。&choice1Bool=true&choice2Bool=false&choice3Bool=false&choice4Bool=false&content=開催可能なイベントのフォーマットは次のうちどれか" http://localhost:8080/api/question
--- static/js/controller/competitionController.js
+++ static/js/controller/competitionController.js
@@ -3,28 +3,24 @@
 var app = angular.module('competitionCtrl', []);
 app.controller('competitionCtrl', ['$scope', '$http', '$sce', '$window', '$mdDialog', 'questionService',
 function($scope, $http, $sce, $window, $mdDialog, questionService) {
-	$scope.questions = [];
-	for (var i = 0; i < 25; i++) {
-		$scope.questions.push(i + 1);
-	}
 	var init = function() {
     	questionService.search().then(function(data) {
-			$scope.list = data;
+			$scope.questions = data;
 		});
 	};
 	init();
-	
+
 	/**
-   * 回答送信ダイアログを表示する 
+   * 回答送信ダイアログを表示する
    */
 	$scope.showConfirm = function(ev) {
     var confirm = $mdDialog.confirm()
           .title('送信してもよろしいですか？')
-          .textContent('回答数 1/25')
+          .textContent('回答数 1/' + $scope.questions.length)
           .targetEvent(ev)
           .ok('OK')
           .cancel('キャンセル');
-    $mdDialog.show(confirm).then(function() {        
+    $mdDialog.show(confirm).then(function() {
       $scope.status = 'You decided to get rid of your debt.';
     }, function() { // cancel
       $scope.status = 'You decided to keep your debt.';
