a49b28c390138327771005133bfb94780bd8a32f
koji takahara <plateau1205@gmail.com>
2016-05-12 15:42:24 +0900

    ユーザが投稿した問題を確認する画面の追加

--- static/js/controller/postedContentController.js
+++ static/js/controller/postedContentController.js
@@ -9,11 +9,8 @@ app.controller('postedContentCtrl', ['$scope', '$http', '$sce', '$window', '$mdD
 function($scope, $http, $sce, $window, $mdDialog, categoryService, questionService, userService) {
   $scope.processed = false;
   $scope.user = {};
-  
-  
   $scope.qFilter = {};
   $scope.sortReverse = false;
-  $scope.searchFish = "";
 
   questionService.search(null, null, null).then(function(data) {
     $scope.postedQuestions = data; 
@@ -21,14 +18,10 @@ function($scope, $http, $sce, $window, $mdDialog, categoryService, questionServi
     console.log(e);
   });
   
-  var init = function() {
-  };
-  init();
-
   /**
    * 取り下げダイアログを表示する
    */
-  $scope.showConfirm = function(ev) {
+  $scope.showConfirm = function(ev, question) {
     $scope.processed = true;
     var confirm = $mdDialog.confirm()
           .title("投稿を取り下げてもよろしいですか？")
@@ -36,35 +29,40 @@ function($scope, $http, $sce, $window, $mdDialog, categoryService, questionServi
           .ok('OK')
           .cancel('キャンセル');
     $mdDialog.show(confirm).then(function() {
-      submission(ev);      
+      submission(ev, question);      
     }, function() { // cancel
       $scope.processed = false;
     });
   };
   
-  var submission = function(ev) {
-    var question = {
-    //   content: $scope.question.content,
-    //   choice1Content: $scope.question.correctAnswer,
-    //   choice2Content: $scope.question.wrongAnswer1,
-    //   choice3Content: $scope.question.wrongAnswer2,
-    //   choice1Bool: true,
-    //   choice2Bool: false,
-    //   choice3Bool: false,
-    //   largeCategoryKey: null,
-    //   mediumCategoryKey: null,
-    //   smallCategoryKey: $scope.question.category,
-    //   userKey: $scope.user.screen_name
-    };
-    questionService.create(question).then(function(data) {
+  var submission = function(ev, question) {
+    question.Status = "TURNDOWN";
+    questionService.update(question).then(function(data) {
       var confirm = $mdDialog.confirm()
-          .title('送信完了しました。')
+          .title('投稿を取り下げました。')
           .targetEvent(ev)
           .ok('OK')
-      $mdDialog.show(confirm).then(function() {
-        $window.location.href = '/';
-      });
-	});
+      $mdDialog.show(confirm);
+	  });
+  };
+  
+  // 問題の確認
+  $scope.showContentModal = function($event, question) {
+    $mdDialog.show({
+      clickOutsideToClose: true,
+      parent: angular.element(document.body),
+      targetEvent: $event,
+      templateUrl: "/view/common/questionModal.html",
+      locals: {
+        question: question
+      },
+      controller: function ModalController($scope, $mdDialog, question) {
+        $scope.q = question;
+        $scope.closeDialog = function() {
+          $mdDialog.hide();
+        };
+      }
+    });
   };
 
 }]);
